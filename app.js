3116c8dcc53bfb9b5416b401f6f97ffefc6ee7ba
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-04 15:49:56 +0300

    geocode is now a proper function

--- app.js
+++ app.js
@@ -8,16 +8,9 @@ const d_sky_config = {
   long: '-122.73',
   lat:'37.8267',
   params:''
-},
-  g_coding_config = {
-  baseUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
-  place: 'Sofia, Bulgaria',
-  params:`access_token=${'pk.eyJ1Ijoic2V3Ym90IiwiYSI6ImNqd2htbDEwbTF6amozeW51dndobDhnZ3QifQ.GchJ-b6ohzKuedU8VGs4mA'}&limit=1`
 };
 
-
 const dark_sky = `${d_sky_config.baseUrl}/${d_sky_config.key}/${d_sky_config.lat},${d_sky_config.long}?${d_sky_config.params}`;
-const geo_coding = `${g_coding_config.baseUrl}/${g_coding_config.place}.json?${g_coding_config.params}`;
 
 // request({url:dark_sky, json:true},(err, res)=>{
 //   if       (err)            console.log('unable to connect to the weather service...');
@@ -25,8 +18,24 @@ const geo_coding = `${g_coding_config.baseUrl}/${g_coding_config.place}.json?${g
 //   else                      console.log(res.body.currently);
 // });
 
-request({url:geo_coding, json:true},(err, res)=>{
-  if       (err)                       console.log('unable to connect to the location service...');
-  else if  (!res.body.features.length) console.log('apparently you did enter the wrong thing..., please check your input carefully');
-  else                                 console.log('latitude: ', res.body.features[0].center[0], 'longitude: ', res.body.features[0].center[1]);
+
+
+
+const geocode = (address, cb) => {
+  const g_coding_config = {
+    baseUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
+    params:`access_token=${'pk.eyJ1Ijoic2V3Ym90IiwiYSI6ImNqd2htbDEwbTF6amozeW51dndobDhnZ3QifQ.GchJ-b6ohzKuedU8VGs4mA'}&limit=1`
+  },
+  geo_coding = `${g_coding_config.baseUrl}/${address}.json?${g_coding_config.params}`;
+  request({url:geo_coding, json:true},(err, res)=>{
+    if       (err)                       cb('Unable to connect to the location service...', undefined);
+    else if  (!res.body.features.length) cb('apparently you did enter the wrong thing..., please check your input carefully', undefined);
+    else                                 cb(undefined, {latitude: res.body.features[0].center[0], longitude: res.body.features[0].center[1]});
+  });
+
+};
+
+geocode('Sofia, Bulgaria', (err, data)=>{
+  console.log('err?', err);
+  console.log('data:', data);
 });
