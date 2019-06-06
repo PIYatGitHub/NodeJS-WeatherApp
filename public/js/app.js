0a3d1ecd6c733a97a18132878207cc10f5cc57de
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-06 21:41:45 +0300

    client side js now fetches the data needed...

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('loading app.js on the client...');
+'use strict';
+
+const url = 'http://localhost:3000/weather?',
+address = 'address=Sofia,Bulgaria';
+
+fetch(url+address).then((response)=>{
+  response.json().then((data)=>{
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data)
+    }
+
+  });
+});
