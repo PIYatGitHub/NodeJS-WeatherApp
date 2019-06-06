8381df4cad7a86465351b1dffd18d2eb29ce94cd
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-06 12:21:13 +0300

    adding setup for views and partials... adding the first partial - a header

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,13 @@
 'use strict';
 const express = require ('express');
+const hbs = require ('hbs');
 const path = require ('path');
 const app = express();
 
-app.set('view engine','hbs');                               //setup for handlebars
-app.set('views',path.join(__dirname, '../templates'));      //setup for handlebars
-app.use(express.static(path.join(__dirname, '../public'))); //setup for static dir to serve!
+app.set('view engine','hbs');                                       //setup for handlebars --> include it!
+app.set('views',path.join(__dirname, '../templates/views'));        //setup for handlebars --> set the views dir to be called templates!
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));//setup for handlebars --> set the partials dir
+app.use(express.static(path.join(__dirname, '../public')));         //setup for static dir to serve, e.g. the public folder...
 
 app.get('', (req, res)=>{
   res.render('index', {
