43eccd3f264614c5082ac703f24c9a564833610d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-06 11:52:30 +0300

    replaced the static index.html with a handlebars template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('', (req, res)=>{
+  res.render('index')
+});
+
 app.get('/weather', (req, res)=>{
   res.send('Your weather view...');
 });
