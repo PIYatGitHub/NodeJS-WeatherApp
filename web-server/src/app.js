66d8f90d6b02e126a1143fba136005bcc25d7fa6
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-06 11:11:46 +0300

    added the hbs package and now using it...

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,7 @@ const express = require ('express');
 const path = require ('path');
 
 const app = express();
+app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/weather', (req, res)=>{
