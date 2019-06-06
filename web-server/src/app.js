e9acb24aa54a66a7420ec638c7c382f76cdc84f4
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-06 10:44:26 +0300

    static pages now all rendering with the express.static func

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,14 +5,6 @@ const path = require ('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/about', (req, res)=>{
-  res.send('About us...');
-});
-
-app.get('/help', (req, res)=>{
-  res.send('Need some help? We have you covered!');
-});
-
 app.get('/weather', (req, res)=>{
   res.send('Your weather view...');
 });
